329663f63a931cdea7c0a93a3af82db9a6b4d788
semenov82 <p.semyon@gmail.com>
2019-04-19 00:43:49 +0300

    Update client.py

--- client.py
+++ client.py
@@ -60,7 +60,7 @@ DC_port = 443
 #client = TelegramClient(fss, api_id, api_hash, proxy=(proxy_ip, proxy_port, secret), connection=connection.tcpmtproxy.ConnectionTcpMTProxy)
 #client.session.set_dc(1, DC1_ip, DC_port)
 client = TelegramClient(fss, api_id, api_hash)
-client.connect()
+client.start()
 if not client.is_user_authorized():
     if nchek1 in nchek: # Проверяем наличие сессии, если её нет, то запрашиваем логин код
         pass 
